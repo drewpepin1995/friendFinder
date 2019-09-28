cf5682a035234666fa403d5f141060c5b2917d6b
Drew Pepin <drewpepin1995@gmail.com>
2019-09-28 17:38:54 -0400

    adding survey.html page

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [
+    {
+        name: "Tim Riggins",
+        photo: "https://images.app.goo.gl/vNyrimaDRUACvHdJ6",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Eric Taylor",
+        photo: "https://images.app.goo.gl/C56TKWVjWrSnQ3Qg9",
+        scores: [4, 3, 2, 3, 1, 5, 3, 2, 3, 4]
+    },
+    {
+        name: "Matt Saracen",
+        photo: "https://images.app.goo.gl/9vAGGy2psw8unfuz7",
+        scores: [1, 1, 1, 1, 1, 4, 1, 2, 1, 1]
+    },
+    {
+        name: "Brian Smash Williams",
+        photo: "https://images.app.goo.gl/jA1dPM25ctP7EZS97",
+        scores: [1, 1, 1, 1, 1, 4, 1, 2, 1, 1]
+    }
+    
+]
