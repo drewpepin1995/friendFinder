152644c848c15a8227012a78b17deb5a26ab3f48
Drew Pepin <drewpepin1995@gmail.com>
2019-09-28 17:41:37 -0400

    adding friends to the friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,13 @@ var friends = [
     {
         name: "Brian Smash Williams",
         photo: "https://images.app.goo.gl/jA1dPM25ctP7EZS97",
-        scores: [1, 1, 1, 1, 1, 4, 1, 2, 1, 1]
+        scores: [5, 2, 2, 4, 2, 3, 5, 4, 5, 1]
+    },
+    {
+        name: "Jason Street",
+        photo: "https://images.app.goo.gl/u9cueDDmsYJCKAb28",
+        scores: [5, 1, 1, 2, 2, 5, 4, 1, 5, 1]
     }
+
     
 ]
